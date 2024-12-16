c332332612cc8e0727e3b912d62aadb663df5521
juliayynn <yunusova1203@gmai.com>
2024-12-16 23:38:15 +0700

    Простое сохранение с помощью SharedPreferences

--- app/src/main/java/com/example/laba_7/PhotoGalleryViewModel.kt
+++ app/src/main/java/com/example/laba_7/PhotoGalleryViewModel.kt
@@ -1,24 +1,29 @@
 package com.bignerdranch.android.photogallery
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.switchMap
 
-class PhotoGalleryViewModel : ViewModel() {
+class PhotoGalleryViewModel(private val app: Application) : AndroidViewModel(app) {
     val galleryItemLiveData : LiveData<List<GalleryItem>>
     private val flickrFetchr = FlickrFetchr()
     private val mutableSearchTerm = MutableLiveData<String>()
     init {
-        mutableSearchTerm.value = "planets"
+        mutableSearchTerm.value = QueryPreferences.getStoredQuery(app)
 
-        galleryItemLiveData =
-            mutableSearchTerm.switchMap() { searchTerm ->
+        galleryItemLiveData = mutableSearchTerm.switchMap() { searchTerm ->
+            if (searchTerm.isBlank()) {
+                flickrFetchr.fetchPhotos()
+            } else {
                 flickrFetchr.searchPhotos(searchTerm)
+            }
             }
     }
 
     fun fetchPhotos(query: String = "") {
+        QueryPreferences.setStoredQuery(app, query)
         mutableSearchTerm.value = query
     }
 }
