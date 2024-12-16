5e25eb4ec0529207e371487ad80d96c76150525d
juliayynn <yunusova1203@gmai.com>
2024-12-16 23:56:24 +0700

    Планирование работы и ладно

--- app/src/main/java/com/example/laba_7/PhotoGalleryFragment.kt
+++ app/src/main/java/com/example/laba_7/PhotoGalleryFragment.kt
@@ -20,6 +20,10 @@ import androidx.lifecycle.ViewModelProviders
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.work.Constraints
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
 
 private const val TAG = "PhotoGalleryFragment"
 
@@ -44,6 +48,15 @@ class PhotoGalleryFragment : Fragment() {
 
         photoGalleryViewModel = ViewModelProviders.of(this).get(PhotoGalleryViewModel::class.java)
         lifecycle.addObserver(thumbnailDownloader.fragmentLifecycleObserver)
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.UNMETERED)
+            .build()
+        val workRequest = OneTimeWorkRequest
+            .Builder(PollWorker::class.java)
+            .setConstraints(constraints)
+            .build()
+        WorkManager.getInstance()
+            .enqueue(workRequest)
     }
 
     override fun onCreateView(
