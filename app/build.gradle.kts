e992d595bdffb023bdcdc25ab1f65a87667d35e1
juliayynn <yunusova1203@gmai.com>
2024-12-16 15:07:52 +0700

    PhotoGallery

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.laba_7"
-        minSdk = 24
+        minSdk = 21
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
