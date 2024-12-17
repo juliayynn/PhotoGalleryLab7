ee5f11695a42db4b8ed2f58822ae54337c4f4410
juliayynn <yunusova1203@gmai.com>
2024-12-17 23:16:15 +0700

    ошибки в фото галерее :(

--- app/src/main/java/com/example/laba_7/PhotoGalleryApplication.kt
+++ app/src/main/java/com/example/laba_7/PhotoGalleryApplication.kt
@@ -1,12 +1,12 @@
 package com.example.laba_7
 
-import com.bignerdranch.android.photogallery.R
-
-
 import android.app.Application
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.content.Context
 import android.os.Build
+import com.bignerdranch.android.photogallery.PhotoRepository
+import com.bignerdranch.android.photogallery.R
 
 
 const val NOTIFICATION_CHANNEL_ID = "flickr_poll"
@@ -14,12 +14,23 @@ const val NOTIFICATION_CHANNEL_ID = "flickr_poll"
 class PhotoGalleryApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        context = applicationContext
+        PhotoRepository.initialize(this)
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val name = getString(R.string.notification_channel_name)
             val importance = NotificationManager.IMPORTANCE_DEFAULT
             val channel = NotificationChannel(NOTIFICATION_CHANNEL_ID, name, importance)
             val notificationManager: NotificationManager = getSystemService(NotificationManager::class.java)
             notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    companion object {
+        private lateinit var context: Context
+
+        fun getAppContext(): Context {
+            return context
         }
     }
 }
